067f92cc2d87b3bc9c7ef3720036bae0783778a2
Nang SeakHeng <seakhengclever@gmail.com>
2025-01-13 16:51:13 +0700

    update eslint

--- src/components/users/AddUserDialog.tsx
+++ src/components/users/AddUserDialog.tsx
@@ -1,18 +1,18 @@
-import { CreateUserInput } from '../../api/services/userService';
-import { useCreateUser } from '../../hooks/queries/useUsers';
+// import { CreateUserInput } from '../../api/services/userService';
+// import { useCreateUser } from '../../hooks/queries/useUsers';
 
-export function AddUserDialog({ onClose }: { onClose: () => void }) {
-  const createUser = useCreateUser();
+// export function AddUserDialog({ onClose }: { onClose: () => void }) {
+//   const createUser = useCreateUser();
 
-  const handleSubmit = async (data: CreateUserInput) => {
-    try {
-      await createUser.mutateAsync(data);
-      onClose();
-    } catch (error) {
-      // Handle error
-      console.error(error);
-    }
-  };
+//   const handleSubmit = async (data: CreateUserInput) => {
+//     try {
+//       await createUser.mutateAsync(data);
+//       onClose();
+//     } catch (error) {
+//       // Handle error
+//       console.error(error);
+//     }
+//   };
 
-  // ... rest of the component
-}
+//   // ... rest of the component
+// }
